f696209f3be69d8699b7c69b92a484f8d0e54043
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2014-08-08 12:31:32 +0300

    Fix assertion for string emptiness check

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,13 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    if str.encoding.dummy?
+      enc = Encoding::UTF_8
+      str = str.force_encoding(enc)
+    elsif str.encoding.ascii_compatible?
+      enc = Encoding::US_ASCII
+    else
+      enc = str.encoding
+    end
+    str.match(Regexp.new('^.+$'.encode(enc), 0))
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      enc_method = enc.dummy? ? 'force_encoding':'encode'
+      assert(RegexpM17N.non_empty?('.'.send(enc_method, enc)))
     end
   end
 end
